0f8bea360be911d8567c1265aa156647aa1b9400
SREENIVASULU RAMANABOINA <112157011+sreeni827@users.noreply.github.com>
2025-03-28 19:57:04 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_tfFInmaw2', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5v2bir1p7oo86oi38d6rnfdbtq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_JqRqeiQ6l', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '62h0teg1p9bnajcqi8cpqjtoq5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
